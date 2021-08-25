2eeeb8aa70159889cab0e4395e5832ed2090e933
Vidhya Selvaraj <87598284+vidhyaa2@users.noreply.github.com>
2021-08-25 09:17:10 +0530

    Add files via upload

---
+++ School1.java
@@ -0,0 +1,16 @@
+
+public class School1{
+	int sage=20;
+	String sname="vidhya";
+	double gpa=8.75;
+
+	public static void main(String[] args) {
+		School1 n=new School1();
+				System.out.println(n.sage);
+		System.out.println(n.sname);
+		System.out.println(n.gpa);
+		// TODO Auto-generated method stub
+
+	}
+
+}
