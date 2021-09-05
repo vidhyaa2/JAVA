bfcbae1d3cf73cd7433e14ffc20040909a0198ba
Vidhya Selvaraj <87598284+vidhyaa2@users.noreply.github.com>
2021-09-05 11:50:47 +0530

    Add files via upload

---
+++ IfDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter age");
+		int age=scanner.nextInt();
+		if(age>=18)
+		{
+			System.out.println("you are a major");
+		}
+		System.out.println("bye");
+		// TODO Auto-generated method stub
+
+	}
+
+}
